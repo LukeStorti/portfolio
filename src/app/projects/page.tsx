f9df3b0855e6d8b55ecc57c34550e8bec0baa575
LUKE STORTI <lukestorti@gmail.com>
2024-05-05 13:17:52 +0200

    added featured project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,20 @@
 import { Dot } from "lucide-react";
 import ProjectsList from "@/components/Projects";
+import FeaturedProject from "@/components/FeaturedProject";
+import { Separator } from "@/components/ui/separator";
 const Projects = () => {
   return (
     <section className="my-12 px-4">
       <div className="container flex flex-col items-center">
+        <FeaturedProject />
+
         <div className="flex items-center space-x-2">
           <Dot size={45} className="text-primary" />
-          <h2 className="text-3xl font-bold">Projects</h2>
+
+          <p className="my-8">Some of the things I have built</p>
           <Dot size={45} className="text-primary" />
         </div>
-        <p>Some of the things I have built</p>
+        <Separator />
         <ProjectsList />
       </div>
     </section>
---
+++ src/components/FeaturedProject.tsx
@@ -0,0 +1,41 @@
+import { Dot } from "lucide-react";
+import ProjectCard from "./ProjectCard";
+
+const FeaturedProject = () => {
+  return (
+    <div className="my-8 w-full flex flex-col items-center">
+      <div className="flex items-center space-x-2">
+        <Dot size={45} className="text-primary" />
+        <h2 className="text-2xl font-bold">Featured Project</h2>
+        <Dot size={45} className="text-primary" />
+      </div>
+      <div className="flex flex-col md:flex-row justify-around mt-8 w-full">
+        <ProjectCard
+          image="/images/Paws.png"
+          title="Paws Animal Adoption"
+          subtitle="A web app that connects animal adoption NPOs and people who are looking to adopt."
+          github="https://github.com/LukeStorti/paws-animal-adoption"
+          link="https://paws-animal-adoption-zz8q.vercel.app/"
+        />
+        <div className="flex flex-col space-y-2 items-start w-full md:w-1/2 my-4">
+          <h3 className="text-2xl mt-2 font-semibold">Paws</h3>
+          <p>
+            Paws is a web app that connects animal adoption NPOs and people looking to adopt. The
+            goal of this project is to help animals in shelters find forever homes by connecting
+            NPOs to people who are looking to adopt.
+          </p>
+          <p>
+            There are many features I would still love to add and hope to add in the future, such
+            as:
+          </p>
+
+          <li>NPO Verification</li>
+          <li>Real time chat</li>
+          <p>Made with Next js, tailwind, shadcn, supabase & Kinde.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProject;
